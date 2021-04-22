c1d002f741e1e4fabf9658541bf6984dfc2a3020
Dimitar Atanasov <dimitar.atanasov@adon.solutions>
2021-04-22 23:49:32 +0100

    Added version requirements

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -4,6 +4,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Allow any 3.x version of the AWS provider
+version = "~> 3.0"
+
+# Allow any 0.14.x version of Terraform
+required_version = ">= 0.14, < 0.15"
+
 resource "aws_iam_user" "example" {
   for_each = toset(var.user_names) #toset converts the list into a set
   name = each.value
